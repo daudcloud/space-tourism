10736fd3385de4f1615695503df3706857788544
unknown <daudsutanto13@gmail.com>
2021-11-05 13:07:29 +0700

    page setup

--- pages/crew.js
+++ pages/crew.js
@@ -1,11 +1,11 @@
 import Header from "../components/Header";
 import Layout from "../components/Layout";
-export default function Home() {
+export default function Crew() {
   return (
     <>
-      <Header title="Frontend Mentor | Space tourism website" />
+      <Header title="Frontend Mentor | Crew" />
       <Layout>
-        <h1>Home</h1>
+        <h1>Crew</h1>
       </Layout>
     </>
   );
--- pages/destination.js
+++ pages/destination.js
@@ -1,11 +1,11 @@
 import Header from "../components/Header";
 import Layout from "../components/Layout";
-export default function Home() {
+export default function Destination() {
   return (
     <>
-      <Header title="Frontend Mentor | Space tourism website" />
+      <Header title="Frontend Mentor | Destination" />
       <Layout>
-        <h1>Home</h1>
+        <h1>Destination</h1>
       </Layout>
     </>
   );
