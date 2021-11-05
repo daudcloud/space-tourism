4c3eb5ee3b9ce648d224c20987f8a0d1e7dac967
unknown <daudsutanto13@gmail.com>
2021-11-05 13:45:55 +0700

    setup page

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,11 +1,22 @@
 import Header from "../Header";
+import Image from "next/image";
 
 export default function Layout({ title, children }) {
   return (
-    <div>
-      <Header title={title} />
-      <header></header>
-      <main>{children}</main>
-    </div>
+    <>
+      <div className="bg-main min-h-screen">
+        <Header title={title} />
+        <header className="pl-6 pt-6 flex">
+          <div className="relative w-10 h-10">
+            <Image
+              src="/assets/shared/logo.svg"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+        </header>
+        <main>{children}</main>
+      </div>
+    </>
   );
 }
